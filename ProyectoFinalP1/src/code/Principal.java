b4b6e736f17473827750d2c291aec55e47294db6
pc <pc@DESKTOP-LQ5HT84>
2018-06-29 11:35:39 -0400

    agrege un atribuo nuevo y los sets y los gets de los atributos

--- ProyectoFinalP1/src/code/Principal.java
+++ ProyectoFinalP1/src/code/Principal.java
@@ -2,8 +2,25 @@ package code;
 
 public class Principal {
 	private String testing;
+	private String nombre;
 
 	public Principal() {
+	}
+
+	public String getTesting() {
+		return testing;
+	}
+
+	public void setTesting(String testing) {
+		this.testing = testing;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 }
