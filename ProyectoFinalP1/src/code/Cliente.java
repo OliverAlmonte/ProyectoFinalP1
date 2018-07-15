7eac3e2f9e84e316c61a43a8c6f85117050bc63d
OliverEduardo <OliverEduardo@OliverAlmonte>
2018-07-15 15:40:12 -0400

    Añadí el código de José

--- ProyectoFinalP1/src/code/Cliente.java
+++ ProyectoFinalP1/src/code/Cliente.java
@@ -1,16 +1,61 @@
 package code;
 
+import java.util.ArrayList;
+
 public class Cliente {
-   private String id;
-	
-	public Cliente() {
-		// TODO Auto-generated constructor stub
+	private String id;
+	   private String nombre;
+	   private String direccion;
+	   private int CantProyectos;
+	  private ArrayList<Proyecto> misProyectos;
+	  
+	public Cliente(String id, String nombre, String direccion, int cantProyectos, ArrayList<Proyecto> misProyectos) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		CantProyectos = cantProyectos;
+		this.misProyectos = misProyectos;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getCantProyectos() {
+		return CantProyectos;
+	}
+
+	public void setCantProyectos(int cantProyectos) {
+		CantProyectos = cantProyectos;
+	}
+
+	public ArrayList<Proyecto> getMisProyectos() {
+		return misProyectos;
+	}
+
+	public void setMisProyectos(ArrayList<Proyecto> misProyectos) {
+		this.misProyectos = misProyectos;
+	}
+	
 }
--- ProyectoFinalP1/src/code/Contrato.java
+++ ProyectoFinalP1/src/code/Contrato.java
@@ -2,8 +2,48 @@ package code;
 
 public class Contrato {
 
-	public Contrato() {
-		// TODO Auto-generated constructor stub
+	private String id;
+	private int tiempoEntrega;
+	private Proyecto proyecto;
+	private Cliente cliente;	
+	public Contrato(String id, int tiempoEntrega, Proyecto proyecto, Cliente cliente) {
+		super();
+		this.id = id;
+		this.tiempoEntrega = tiempoEntrega;
+		this.proyecto = proyecto;
+		this.cliente = cliente;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getTiempoEntrega() {
+		return tiempoEntrega;
+	}
+
+	public void setTiempoEntrega(int tiempoEntrega) {
+		this.tiempoEntrega = tiempoEntrega;
+	}
+
+	public Proyecto getProyecto() {
+		return proyecto;
+	}
+
+	public void setProyecto(Proyecto proyecto) {
+		this.proyecto = proyecto;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 
 }
