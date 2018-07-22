d6bc50547a88bef81e190f95c5614a076b01c15a
pc <pc@DESKTOP-LQ5HT84>
2018-07-21 23:20:01 -0400

    agrege el Jframe que conectara todas los JDialogs

---
+++ ProyectoFinalP1/src/visual/Principal.java
@@ -0,0 +1,115 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPane;
+    private Dimension dim;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal() {
+		setResizable(false);
+		setTitle("Magic Software");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		dim = super.getToolkit().getScreenSize();
+		super.setSize(dim.width, (dim.height-50));
+		setLocationRelativeTo(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnRegistrar = new JMenu("Registrar");
+		menuBar.add(mnRegistrar);
+		
+		JMenuItem mntmCliente = new JMenuItem("Cliente");
+		mntmCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnRegistrar.add(mntmCliente);
+		
+		JMenuItem mntmEmpleado = new JMenuItem("Empleado");
+		mntmEmpleado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnRegistrar.add(mntmEmpleado);
+		
+		JMenuItem mntmProyecto = new JMenuItem("Proyecto");
+		mntmProyecto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegistrarProyecto proyecto = new RegistrarProyecto();
+				proyecto.setModal(true);
+				proyecto.setVisible(true);
+			}
+		});
+		mnRegistrar.add(mntmProyecto);
+		
+		JMenu mnListar = new JMenu("Listar");
+		menuBar.add(mnListar);
+		
+		JMenuItem mntmCliente_1 = new JMenuItem("Cliente");
+		mntmCliente_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//aqui ira el listado de cliente
+			}
+		});
+		mnListar.add(mntmCliente_1);
+		
+		JMenuItem mntmEmpleado_1 = new JMenuItem("Empleado");
+		mntmEmpleado_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListarEmpleados emp2 = new ListarEmpleados();
+				emp2.setModal(true);
+				emp2.setVisible(true);
+			}
+		});
+		mnListar.add(mntmEmpleado_1);
+		
+		JMenuItem mntmProyecto_1 = new JMenuItem("Proyecto");
+		mntmProyecto_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnListar.add(mntmProyecto_1);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
--- ProyectoFinalP1/src/visual/RegistrarProyecto.java
+++ ProyectoFinalP1/src/visual/RegistrarProyecto.java
@@ -28,15 +28,7 @@ public class RegistrarProyecto extends JDialog {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		try {
-			RegistrarProyecto dialog = new RegistrarProyecto();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	
 
 	/**
 	 * Create the dialog.
