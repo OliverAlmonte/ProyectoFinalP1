5792e539aaff4110e1b9ddfdcf760c830caa3ba2
OliverEduardo <OliverEduardo@OliverAlmonte>
2018-07-22 08:22:32 -0400

    visual

--- ProyectoFinalP1/src/visual/AgregarEmpleado.java
+++ ProyectoFinalP1/src/visual/AgregarEmpleado.java
@@ -1,199 +1,175 @@
 package visual;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
-
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 import java.awt.Font;
+import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.SpinnerListModel;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.ChangeEvent;
 import javax.swing.border.TitledBorder;
-import java.awt.Color;
-import java.awt.Toolkit;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("rawtypes")
 public class AgregarEmpleado extends JDialog {
 
-	
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private final JPanel contentPanel = new JPanel();
 	private JTextField txtID;
 	private JTextField txtNombre;
 	private JTextField txtDireccion;
-	private JTextField txtLenguaje;
 	private String cargo;
-	JPanel DynamicPanel = new JPanel();
-	JLabel lblLenguaje = new JLabel("Lenguaje:");
+	JPanel panelInfo = new JPanel();
+	JComboBox cbxCargo = new JComboBox();
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AgregarEmpleado dialog = new AgregarEmpleado();
-					dialog.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		try {
+			AgregarEmpleado dialog = new AgregarEmpleado();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the dialog.
 	 */
+	@SuppressWarnings("unchecked")
 	public AgregarEmpleado() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\OliverEduardo\\Downloads\\WEB_DEVELOPMENT_02-512.png"));
-		setBackground(Color.BLACK);
-		setForeground(Color.BLACK);
-		setTitle("Registrar Nuevo Empleado");
-		setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
-		setBounds(100, 100, 534, 308);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 507, 440);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
 		
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setLayout(null);
 		
 		JLabel lblID = new JLabel("ID:");
 		lblID.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblID.setBounds(10, 24, 46, 14);
-		panel.add(lblID);
-		
-		txtLenguaje = new JTextField();
-		txtLenguaje.setBounds(73, 22, 108, 20);
-		DynamicPanel.setBorder(new TitledBorder(null, "Informaci\u00F3n espec\u00EDfica del tipo de empleado", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		DynamicPanel.add(txtLenguaje);
-		txtLenguaje.setColumns(10);
-		txtLenguaje.setVisible(false);
+		contentPanel.add(lblID);
 		
 		txtID = new JTextField();
 		txtID.setEditable(false);
-		txtID.setBounds(34, 21, 104, 20);
-		panel.add(txtID);
+		txtID.setBounds(37, 22, 86, 20);
+		contentPanel.add(txtID);
 		txtID.setColumns(10);
 		
 		JLabel lblNombre = new JLabel("Nombre:");
 		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNombre.setBounds(159, 24, 64, 14);
-		panel.add(lblNombre);
+		lblNombre.setBounds(10, 115, 68, 14);
+		contentPanel.add(lblNombre);
 		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(212, 22, 286, 20);
-		panel.add(txtNombre);
+		txtNombre.setBounds(75, 113, 408, 20);
+		contentPanel.add(txtNombre);
 		txtNombre.setColumns(10);
 		
 		JLabel lblDireccion = new JLabel("Direcci\u00F3n:");
 		lblDireccion.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblDireccion.setBounds(10, 65, 80, 14);
-		panel.add(lblDireccion);
+		lblDireccion.setBounds(10, 159, 68, 14);
+		contentPanel.add(lblDireccion);
 		
 		txtDireccion = new JTextField();
-		txtDireccion.setBounds(75, 63, 423, 20);
-		panel.add(txtDireccion);
+		txtDireccion.setBounds(75, 156, 408, 20);
+		contentPanel.add(txtDireccion);
 		txtDireccion.setColumns(10);
-		
-		JLabel lblSexo = new JLabel("Sexo:");
-		lblSexo.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblSexo.setBounds(10, 107, 46, 14);
-		panel.add(lblSexo);
-		
-		JRadioButton rdbtnM = new JRadioButton("M");
-		rdbtnM.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		rdbtnM.setBounds(52, 104, 38, 23);
-		panel.add(rdbtnM);
-		
-		JRadioButton rdbtnF = new JRadioButton("F\r\n");
-		rdbtnF.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		rdbtnF.setBounds(92, 104, 40, 23);
-		panel.add(rdbtnF);
 		
 		JLabel lblEdad = new JLabel("Edad:");
 		lblEdad.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblEdad.setBounds(177, 107, 46, 14);
-		panel.add(lblEdad);
+		lblEdad.setBounds(10, 209, 46, 14);
+		contentPanel.add(lblEdad);
+		
+		JRadioButton rdbtnM = new JRadioButton("M");
+		rdbtnM.setBounds(208, 206, 39, 23);
+		contentPanel.add(rdbtnM);
+		
+		JRadioButton rdbtnF = new JRadioButton("F");
+		rdbtnF.setBounds(248, 206, 33, 23);
+		contentPanel.add(rdbtnF);
+		
+		JLabel lblSexo = new JLabel("Sexo:");
+		lblSexo.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblSexo.setBounds(171, 210, 46, 14);
+		contentPanel.add(lblSexo);
 		
 		JSpinner spnEdad = new JSpinner();
-		spnEdad.setModel(new SpinnerNumberModel(1, 1, 120, 1));
+		spnEdad.setModel(new SpinnerNumberModel(new Integer(18), new Integer(18), null, new Integer(1)));
 		spnEdad.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		spnEdad.setBounds(215, 104, 64, 20);
-		panel.add(spnEdad);
+		spnEdad.setBounds(49, 207, 56, 20);
+		contentPanel.add(spnEdad);
 		
 		JLabel lblCargo = new JLabel("Cargo:");
 		lblCargo.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblCargo.setBounds(336, 107, 46, 14);
-		panel.add(lblCargo);
+		lblCargo.setBounds(316, 210, 46, 14);
+		contentPanel.add(lblCargo);
 		
-		JSpinner spnCargo = new JSpinner();
-		spnCargo.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				cargo = (String) spnCargo.getValue();
-				if(cargo=="Programador") {
-					txtLenguaje.setVisible(true);	
-					lblLenguaje.setVisible(true);
-				}
-				else {
-					txtLenguaje.setVisible(false);	
-					lblLenguaje.setVisible(false);
-				}
+		
+		panelInfo.setBorder(new TitledBorder(null, "Informaci\u00F3n Espec\u00EDfica", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelInfo.setBounds(0, 253, 493, 67);
+		panelInfo.setVisible(false);
+		
+		
+		cbxCargo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			cargo=(String) cbxCargo.getSelectedItem();
+			if(cargo=="Programador") {
+				panelInfo.setVisible(true);
+			}
+			else {
+				panelInfo.setVisible(false);
+			}
 			}
 		});
-		spnCargo.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		spnCargo.setModel(new SpinnerListModel(new String[] {"<Elegir>", "Jefe de Proyecto", "Programador", "Planificador", "Dise\u00F1ador"}));
-		spnCargo.setBounds(381, 104, 117, 20);
-		panel.add(spnCargo);
+		cbxCargo.setModel(new DefaultComboBoxModel(new String[] {"<Elegir>", "Programador", "Jefe de Proyecto", "Planificador", "Dise\u00F1ador"}));
+		cbxCargo.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		cbxCargo.setBounds(360, 207, 123, 20);
+		contentPanel.add(cbxCargo);
+		contentPanel.add(panelInfo);
+		panelInfo.setLayout(null);
 		
-		
-		JPanel buttonPane = new JPanel();
-		buttonPane.setBounds(0, 226, 504, 33);
-		panel.add(buttonPane);
-		buttonPane.setLayout(null);
-		
-		JButton button = new JButton("Registrar");
-		button.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button.setBounds(308, 9, 90, 23);
-		button.setActionCommand("OK");
-		buttonPane.add(button);
-		
-		JButton button_1 = new JButton("Cancelar");
-		button_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button_1.setBounds(408, 9, 86, 23);
-		button_1.setActionCommand("Cancel");
-		buttonPane.add(button_1);
-		
-		JLabel lblCamposObligatorios = new JLabel("Todos los campos son obligatorios.");
-		lblCamposObligatorios.setForeground(Color.RED);
-		lblCamposObligatorios.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 9));
-		lblCamposObligatorios.setBounds(10, 9, 180, 14);
-		buttonPane.add(lblCamposObligatorios);
-		
-		
-		DynamicPanel.setBounds(0, 152, 505, 65);
-		panel.add(DynamicPanel);
-		DynamicPanel.setLayout(null);
-		
-		
+		JLabel lblLenguaje = new JLabel("Lenguaje:");
 		lblLenguaje.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblLenguaje.setBounds(10, 24, 71, 14);
-		DynamicPanel.add(lblLenguaje);
-		lblLenguaje.setVisible(false);
+		lblLenguaje.setBounds(10, 26, 78, 14);
+		panelInfo.add(lblLenguaje);
 		
+		JComboBox cbxLenguaje = new JComboBox();
+		cbxLenguaje.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		cbxLenguaje.setModel(new DefaultComboBoxModel(new String[] {"<Elegir>", "C", "C++", "Java", "Python", "Web"}));
+		cbxLenguaje.setBounds(82, 24, 91, 20);
+		panelInfo.add(cbxLenguaje);
 		
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Registrar");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
 	}
 }
