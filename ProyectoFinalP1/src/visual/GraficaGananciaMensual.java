37aa420168a332613b50524df374df146b17115a
pc <pc@192.168.1.8>
2018-08-02 20:46:32 -0400

    la grafica

---
+++ ProyectoFinalP1/src/visual/GraficaGananciaMensual.java
@@ -0,0 +1,89 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import code.Empresa;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class GraficaGananciaMensual extends JDialog {
+	 JPanel panel;
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	
+
+	/**
+	 * Create the dialog.
+	 */
+	public GraficaGananciaMensual() {
+		setBounds(100, 100, 863, 494);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+		init();
+	}
+	
+	 private void init() {
+	        panel = new JPanel();
+	        getContentPane().add(panel);
+	        // Fuente de Datos
+	        double[] cant = Empresa.getInstance().gananciasByMes();
+	        
+	        DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
+	        line_chart_dataset.addValue(cant[0], "ganancias", "Enero");
+	        line_chart_dataset.addValue(cant[1], "ganancias", "Feb");
+	        line_chart_dataset.addValue(cant[2], "ganancias", "Marzo");
+	        line_chart_dataset.addValue(cant[3], "ganancias", "Abril");
+	        line_chart_dataset.addValue(cant[4], "ganancias", "Mayo");
+	        line_chart_dataset.addValue(cant[5], "ganancias", "Junio");
+	        line_chart_dataset.addValue(cant[6], "ganancias", "Julio");
+	        line_chart_dataset.addValue(cant[7], "ganancias", "Ag");
+	        line_chart_dataset.addValue(cant[8], "ganancias", "Sep");
+	        line_chart_dataset.addValue(cant[9], "ganancias", "Oct"); 
+	        line_chart_dataset.addValue(cant[10], "ganancias", "Nov");   
+	        line_chart_dataset.addValue(cant[11], "ganancias", "Dic");
+	        // Creando el Grafico
+	        JFreeChart chart=ChartFactory.createLineChart("Comparacion mensual a Escala",
+	                "Mes","Dinero Facturado por la Empresa",line_chart_dataset,PlotOrientation.VERTICAL,
+	                true,true,false);  
+	        
+	        // Mostrar Grafico
+	        ChartPanel chartPanel = new ChartPanel(chart);
+	        panel.add(chartPanel);
+	    }
+    
+
+}
